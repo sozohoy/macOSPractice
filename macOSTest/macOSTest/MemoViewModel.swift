31ef6226dcdeb4eb263c2d44f6f63cca6109b76e
sozohoy <sozohoy@gmail.com>
2023-07-14 15:42:16 +0900

    cloudkit에서 데이터 받아오기 뚝딱

--- macOSTest/macOSTest/MemoViewModel.swift
+++ macOSTest/macOSTest/MemoViewModel.swift
@@ -8,47 +8,101 @@
 import Foundation
 import CloudKit
 
-struct Memo {
+struct Memo: Identifiable {
     let id: UUID
     let createdAt: Date
     let text: String
 }
 
+struct Menu: Identifiable {
+    let id = UUID().uuidString
+    let name: String
+}
+
+final class ListViewModel: ObservableObject {
+    var menu: [Menu] = []
+    
+    init() {
+        menu = [
+            Menu(name: "아이디어 노트"),
+            Menu(name: "보관함"),
+            Menu(name: "휴지통")
+        ]
+    }
+    
+    
+}
+
 final class CloudManager: ObservableObject {
     
     private var container = CKContainer(identifier: "iCloud.macOSTest")
+    @Published var memo: [Memo] = []
 
-    
-    func fetchMemo() {
+//    async throws -> [Memo]
+    func fetchMemo(_ completion: @escaping ([Memo]) -> ()) {
+        var memo: [Memo] = []
         let predicate = NSPredicate(value: true)
         let query = CKQuery(recordType: "Memo", predicate: predicate)
+        query.sortDescriptors = [NSSortDescriptor(key: "createdAt", ascending: true)]
         let operation = CKQueryOperation(query: query)
         operation.database = container.privateCloudDatabase
-
+        
+        operation.queryResultBlock = { result in
+            switch result {
+            case .success(_):
+                completion(memo)
+            case .failure(let error):
+                print("@Log - \(error.localizedDescription)")
+            }
+        }
+        
         operation.recordMatchedBlock = { resultId, result in
             switch result {
             case .success(let record):
                 guard let text = record["text"] as? String,
                       let createdAt = record["createdAt"] as? Date
                 else { return }
-                print("@Log record - \(text), \(createdAt)")
+                print("@Log - \(text), \(createdAt)")
+                memo.append(Memo(id: UUID(), createdAt: createdAt, text: text))
+                
             case .failure(let error):
                 print("@Log error - \(error.localizedDescription)")
             }
+//            completion(memo)
         }
-
-        operation.queryResultBlock = { result in
-            print("@Log queryResult - \(result)")
-            switch result {
-            case .success(let a):
-                print("@Log a - \(a)")
-            case .failure(let error):
-                print("@Log error - \(error.localizedDescription)")
-            }
-        }
-
         operation.start()
     }
+    
+    func temp() {
+        fetchMemo { memo in
+            self.memo = memo
+        }
+    }
+    
+//    operation.recordMatchedBlock = { resultId, result in
+//        switch result {
+//        case .success(let record):
+//            guard let text = record["text"] as? String,
+//                  let createdAt = record["createdAt"] as? Date
+//            else { return }
+//            print("@Log - \(text), \(createdAt)")
+//            memo.append(Memo(id: UUID(), createdAt: createdAt, text: text))
+//        case .failure(let error):
+//            print("@Log error - \(error.localizedDescription)")
+//        }
+//    }
+//    var chartRequest = MusicCatalogChartsRequest(types: [Song.self])
+//    chartRequest.limit = 10
+//    let chartResponse = try await chartRequest.response().songCharts[0].items
+//    return chartResponse.songToMusic()
+
+    
+//    func getMemo(text: String, createdAt: Date) -> [Memo] {
+//        var memo: [Memo] = []
+//        memo.append(Memo(id: UUID(), createdAt: createdAt, text: text))
+//        print("@Log - \(memo)")
+//        return memo
+//    }
 
         
 }
